cc30c2d7ec7c40e4a3a4c2fcdcc34fd81bafdaaa
Martin Sjölund <martin.sjolund@liu.se>
2019-01-07 14:29:03 +0100

    Use MetaModelica LLVM stubs when LLVM is not found
    This avoids creating the huge number of C stubs used by EXT_LLVM and is
    not the standard procedure used for optional code in the compiler.

--- Compiler/runtime/ext_llvm_stubs.cpp
+++ Compiler/runtime/ext_llvm_stubs.cpp
@@ -1,132 +1,99 @@
-#ext_llvm.h
+#include "ext_llvm.hpp"
+#include "errorext.h"
+
+#define LLVM_STUB() {c_add_message(NULL,0,ErrorType_scripting,ErrorLevel_error,"LLVM support not enabled.",NULL,0);MMC_THROW();}
+#define STUB_BINARY(NAME) int NAME(const char*,const char*,const char*){LLVM_STUB(); return 0;}
+#define STUB_UNARY(NAME) int NAME(const char*,const char*){LLVM_STUB(); return 0;}
 
 extern "C"
 {
-  void initGen(const char *) {}
+  void initGen(const char *) {LLVM_STUB();}
 
   /* Functions called externally from Compiler/LLVM/MidToLLVM.mo */
   void* runJIT(void *valLst) {
+    LLVM_STUB();
     return nullptr;
   }
 
   int createFunctionProtArg(const uint8_t,const char *) {
+    LLVM_STUB();
     return 0;
   }
 
-  void finishGen() {}
+  void finishGen() {LLVM_STUB();}
 
-  void startFuncGen(const char *) {}
+  void startFuncGen(const char *) {LLVM_STUB();}
 
-  int genFunctionType() {return 0;}
+  int genFunctionType() {LLVM_STUB(); return 0;}
 
-  int createFunctionPrototype(const char *) {return NULL;}
+  int createFunctionPrototype(const char *) {LLVM_STUB(); return NULL;}
 
-  int createFunctionBody(const char *) {return NULL;}
+  int createFunctionBody(const char *) {LLVM_STUB(); return NULL;}
 
-  int setNewActiveBlock(const modelica_integer) {return 0;}
+  int setNewActiveBlock(const modelica_integer) {LLVM_STUB(); return 0;}
 
   /*Functions to create misc instructions */
-  int createStoreVarInst(const char *src, const char *) {return 0;}
+  int createStoreVarInst(const char *src, const char *) {LLVM_STUB(); return 0;}
 
   /*Function related */
-  int createReturn(const char *){return 0;}
+  int createReturn(const char *){LLVM_STUB(); return 0;}
 
-  int createExit(const modelica_integer exit_id){return 0;}
+  int createExit(const modelica_integer exit_id){LLVM_STUB(); return 0;}
 
-  int createCallArg(const char *){return 0;}
+  int createCallArg(const char *){LLVM_STUB(); return 0;}
 
-  int createCall(const char *, const uint8_t, const char *, modelica_boolean, modelica_boolean){return 0;}
+  int createCall(const char *, const uint8_t, const char *, modelica_boolean, modelica_boolean){LLVM_STUB(); return 0;}
 
   /* Unary instructions */
-  int createIUminus(const char *src,const char *){return 0;}
-
-  int createDUminus(const char *src, const char *){return 0;}
-
-  int createNot(const char *src,const char *){return 0;}
+  STUB_UNARY(createIUminus)
+  STUB_UNARY(createDUminus)
+  STUB_UNARY(createNot)
 
   //Move instructions, e.g casts
-  int createIntToDouble(const char*,const char*){return 0;}
-
-  int createDoubleToInt(const char*,const char*){return 0;}
-
-  int createIntToBool(const char*,const char*){return 0;}
-
-  int createBoolToInt(const char*,const char*){return 0;}
-
-  int createIntToMeta(const char*,const char*){return 0;}
-
-  int createMetaToInt(const char*,const char*){return 0;}
-
-  int createDoubleToBool(const char*,const char*){return 0;}
-
-  int createBoolToDouble(const char*,const char*){return 0;}
-
-  int createDoubleToMeta(const char*,const char*){return 0;}
-
-  int createMetaToDouble(const char*,const char*){return 0;}
-
-  int createDoubleToMeta(const char*,const char*){return 0;}
+  STUB_UNARY(createIntToDouble)
+  STUB_UNARY(createDoubleToInt)
+  STUB_UNARY(createIntToBool)
+  STUB_UNARY(createBoolToInt)
+  STUB_UNARY(createIntToMeta)
+  STUB_UNARY(createMetaToInt)
+  STUB_UNARY(createDoubleToBool)
+  STUB_UNARY(createBoolToDouble)
+  STUB_UNARY(createMetaToDouble)
+  STUB_UNARY(createDoubleToMeta)
 
   /* Binary instructions */
-  int createIAdd(const char *, const char *,const char *){return 0;}
+  STUB_BINARY(createIAdd)
+  STUB_BINARY(createISub)
+  STUB_BINARY(createIMul)
+  STUB_BINARY(createIDiv)
+  STUB_BINARY(createIPow)
+  STUB_BINARY(createILess)
+  STUB_BINARY(createILessEq)
+  STUB_BINARY(createIEqual)
+  STUB_BINARY(createINequal)
+  STUB_BINARY(createDAdd)
+  STUB_BINARY(createDSub)
+  STUB_BINARY(createDMul)
+  STUB_BINARY(createDDiv)
+  STUB_BINARY(createDPow)
+  STUB_BINARY(createDLess)
+  STUB_BINARY(createDLessq)
+  STUB_BINARY(createDEqual)
+  STUB_BINARY(createDNequal)
+  STUB_BINARY(createBAdd)
+  STUB_BINARY(createBSub)
+  STUB_BINARY(createBMul)
+  STUB_BINARY(createBBiv)
+  STUB_BINARY(createBPow)
+  STUB_BINARY(createBLess)
+  STUB_BINARY(createBLESSQ)
+  STUB_BINARY(createBEqual)
+  STUB_BINARY(createBNequal)
+  STUB_BINARY(createPow)
 
-  int createISub(const char *, const char *,const char *){return 0;}
+  int storeLiteralInt64(const modelica_integer,const char*){LLVM_STUB(); return 0;}
 
-  int createIMul(const char *, const char *,const char *){return 0;}
+  int storeLiteralReal (const double,const char *){LLVM_STUB(); return 0;}
 
-  int createIDiv(const char *, const char *,const char *){return 0;}
-
-  int createIPow(const char *, const char *,const char *){return 0;}
-
-  int createILess(const char *, const char *,const char *){return 0;}
-
-  int createILESSQ(const char *, const char *,const char *){return 0;}
-
-  int createIEqual(const char *, const char *,const char *){return 0;}
-
-  int createINequal(const char *, const char *,const char *){return 0;}
-
-  int createDAdd(const char* , const char *, const char *){return 0;}
-
-  int createDSub(const char *, const char *, const char *){return 0;}
-
-  int createDMul(const char *, const char *, const char *){return 0;}
-
-  int createDDiv(const char *, const char *, const char *){return 0;}
-
-  int createDPow(const char *, const char *, const char *){return 0;}
-
-  int createDLess(const char *, const char *, const char *){return 0;}
-
-  int createDLessq(const char *, const char *, const char *){return 0;}
-
-  int createDEqual(const char *, const char *, const char *){return 0;}
-
-  int createDNequal(const char *, const char *, const char *){return 0;}
-
-  int createBAdd(const char * , const char *, const char *){return 0;}
-
-  int createBSub(const char *, const char *, const char *){return 0;}
-
-  int createBMul(const char *, const char *, const char *){return 0;}
-
-  int createBBiv(const char *, const char *, const char *){return 0;}
-
-  int createBPow(const char *, const char *, const char *){return 0;}
-
-  int createBLess(const char *, const char *, const char *){return 0;}
-
-  int createBLESSQ(const char *, const char *, const char *){return 0;}
-
-  int createBEqual(const char *, const char *, const char *){return 0;}
-
-  int createBNequal(const char *, const char *, const char *){return 0;}
-
-  int createPow(const char *, const char *, const char *){return 0;}
-
-  int storeLiteralInt64 (const modelica_integer,const char*){return 0;}
-
-  int storeLiteralReal (const double,const char *){return 0;}
-
-  int storeLiteralIntForPtrTy(const uint64_t addr,const char *dest){return 0;}
+  int storeLiteralIntForPtrTy(const uint64_t addr,const char *dest){LLVM_STUB(); return 0;}
 }
